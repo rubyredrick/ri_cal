6319c5e20aca56c34f4ab0b5c590e1341d8f0a4d
Rick DeNatale <rick.denatale@gmail.com>
2008-10-28 18:12:32 -0400

    some event properties

--- lib/vevent.rb
+++ lib/vevent.rb
@@ -1,6 +1,27 @@
-class Rfc2445::Vevent
-  
-  def self.from_parser(parser)
+require File.expand_path(File.join(File.dirname(__FILE__), 'ventity'))
+
+module Rfc2445
+  class Vevent < Ventity
+    
+    attr_accessor :attach_property, :categories_property
+    
+    def self.entity_name
+      "VEVENT"
+    end
+
+
+
+    def process_line(parser, line)
+      case line[:name]
+      when "ATTACH"
+        self.attach_property = VTextProperty.new(line)
+        # TO-DO - should be an array property
+      when "CATEGORIES"
+        self.categories_property = VTextProperty.new(line) 
+      else 
+        self.add_x_property(VTextProperty.new(line))
+      end
+    end
+
   end
-  
 end
