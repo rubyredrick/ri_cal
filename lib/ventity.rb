145e47cc65e6298541aee3fdfc758dec45ce363b
Rick DeNatale <rick.denatale@gmail.com>
2008-10-29 16:42:55 -0400

    refactored and added array property

--- lib/ventity.rb
+++ lib/ventity.rb
@@ -1,8 +1,48 @@
-require File.expand_path(File.join(File.dirname(__FILE__), 'vtextproperty'))
+require File.expand_path(File.join(File.dirname(__FILE__), 'v_text_property'))
 
 module Rfc2445
   class Ventity
 
+    def self.property_map
+      @property_map ||= {}
+    end
+
+    def self.text_properties(*names)
+      names.each do |name|
+        property(name)
+      end
+    end
+
+    def self.array_properties(*names)
+      names.each do |name|
+        property(name, VArrayProperty)
+      end
+    end
+    class << self
+      alias_method :text_property, :text_properties
+      alias_method :array_property, :array_properties
+    end
+
+
+    def self.property(name, type = VTextProperty, &block)
+      property = "#{name.downcase}_property"
+      attr_accessor property.to_sym
+
+      unless instance_methods(false).include?(name.downcase)
+        class_eval "def #{name.downcase};#{name.downcase}_property.value;end"
+      end
+
+      if block_given?
+        evaluator = lambda(&block)
+      else
+        evaluator = lambda {|line| type.new(line) }
+      end
+      self.property_map[name.upcase] = lambda {|entity, line| entity.send("#{property}=".to_sym, evaluator.call(line)) }
+    end
+
+    def self.entity_name
+      @entity_name ||= to_s.split("::").last.upcase
+    end
 
     def self.from_parser(parser)
       entity = self.new
@@ -13,15 +53,25 @@ module Rfc2445
       end
       entity
     end
-    
-    
+
+    def process_line(parser, line)
+      creator = self.class.property_map[line[:name]]
+      if creator
+        creator.call(self, line)
+      else 
+        self.add_x_property(VTextProperty.new(line))
+      end
+    end
+
+
+
     def x_properties
       @x_properties ||= {}
     end
-    
+
     def add_x_property(prop)
       x_properties[prop.name] = prop
     end
-    
+
   end
 end
--- lib/vevent.rb
+++ lib/vevent.rb
@@ -2,26 +2,8 @@ require File.expand_path(File.join(File.dirname(__FILE__), 'ventity'))
 
 module Rfc2445
   class Vevent < Ventity
+    text_property "attach"
+    array_property "categories"
     
-    attr_accessor :attach_property, :categories_property
-    
-    def self.entity_name
-      "VEVENT"
-    end
-
-
-
-    def process_line(parser, line)
-      case line[:name]
-      when "ATTACH"
-        self.attach_property = VTextProperty.new(line)
-        # TO-DO - should be an array property
-      when "CATEGORIES"
-        self.categories_property = VTextProperty.new(line) 
-      else 
-        self.add_x_property(VTextProperty.new(line))
-      end
-    end
-
-  end
+   end
 end
