d0c00728d1851f55205b4699390185f9de9bda8a
Rick DeNatale <rick.denatale@gmail.com>
2008-12-15 17:36:41 -0500

    initial by_xxx rules

--- spec/lib/recurrence_rule_spec.rb
+++ spec/lib/recurrence_rule_spec.rb
@@ -30,28 +30,72 @@ describe RiCal::RecurrenceRule do
       @it.should_not be_valid            
       @it.errors.should include("COUNT and UNTIL cannot both be specified")
     end
+    
+    describe "interval parameter" do 
+      
+      # p 42
+      it "should default to 1" do
+        RiCal::RecurrenceRule.new(:freq => "daily").interval.should == 1
+      end 
+      
+      it "should accept an explicit value" do
+        RiCal::RecurrenceRule.new(:freq => "daily", :interval => 42).interval.should == 42
+      end
+      
+      it "should reject a negative value" do
+        @it = RiCal::RecurrenceRule.new(:freq => "daily", :interval => -1)
+        @it.should_not be_valid
+      end
+    end
+    
+    describe "by_second parameter" do
+
+      it "should accept a single integer" do
+        @it = RiCal::RecurrenceRule.new(:freq => "daily", :by_second => 10)
+        @it.send(:by_list)[:by_second].should == [10]
+      end
+
+      it "should accept an array of integers" do
+        @it = RiCal::RecurrenceRule.new(:freq => "daily", :by_second => [10, 20])
+        @it.send(:by_list)[:by_second].should == [10, 20]
+      end
+    end
+
+    
+    describe "by_minute parameter" do
+
+      it "should accept a single integer" do
+        @it = RiCal::RecurrenceRule.new(:freq => "daily", :by_minute => 10)
+        @it.send(:by_list)[:by_minute].should == [10]
+      end
+
+      it "should accept an array of integers" do
+        @it = RiCal::RecurrenceRule.new(:freq => "daily", :by_minute => [10, 20])
+        @it.send(:by_list)[:by_minute].should == [10, 20]
+      end
+    end
+
+    describe "freq accessors" do
+      before(:each) do
+        @it = RiCal::RecurrenceRule.new(:freq => 'daily')
+      end
+
+      it "should convert the initial value to uppercase" do
+        @it.freq.should == 'DAILY'
+      end
+
+      it "should convert the setter value to uppercase " do
+        @it.freq = 'weekly'
+        @it.freq.should == 'WEEKLY'
+      end
+
+      it "should not accept an invalid value" do
+        @it.freq = 'bogus'
+        @it.should_not be_valid
+      end
+    end
   end
-  
-  describe "freq accessors" do
-    before(:each) do
-      @it = RiCal::RecurrenceRule.new(:freq => 'daily')
-    end
-    
-    it "should convert the initial value to uppercase" do
-      @it.freq.should == 'DAILY'
-    end
-    
-    it "should convert the setter value to uppercase " do
-      @it.freq = 'weekly'
-      @it.freq.should == 'WEEKLY'
-    end
-    
-    it "should not accept an invalid value" do
-      @it.freq = 'bogus'
-      @it.should_not be_valid
-    end
-  end
-  
+
   describe "initialized from string" do
   end
 end
