6d4b080609d62ee569187070dc03186260601092
Rick DeNatale <rick.denatale@gmail.com>
2008-12-15 20:05:11 -0500

    all by_xxx rules can now be set on instantiation

--- spec/lib/recurrence_rule_spec.rb
+++ spec/lib/recurrence_rule_spec.rb
@@ -2,6 +2,15 @@ require File.join(File.dirname(__FILE__), %w[.. spec_helper])
 
 require 'lib/recurrence_rule'
 
+AnyMonday = RiCal::RecurrenceRule::RecurringDay.new("MO")
+AnyWednesday = RiCal::RecurrenceRule::RecurringDay.new("WE")
+FirstOfMonth = RiCal::RecurrenceRule::RecurringMonthDay.new(1)
+TenthOfMonth = RiCal::RecurrenceRule::RecurringMonthDay.new(10)
+FirstOfYear = RiCal::RecurrenceRule::RecurringYearDay.new(1)
+TenthOfYear = RiCal::RecurrenceRule::RecurringYearDay.new(10)
+SecondWeekOfYear = RiCal::RecurrenceRule::RecurringNumberedWeek.new(2)
+LastWeekOfYear = RiCal::RecurrenceRule::RecurringNumberedWeek.new(-1)
+
 # rfc 2445 4.3.10 p.40
 describe RiCal::RecurrenceRule do
   
@@ -60,9 +69,14 @@ describe RiCal::RecurrenceRule do
         @it = RiCal::RecurrenceRule.new(:freq => "daily", :by_second => [10, 20])
         @it.send(:by_list)[:by_second].should == [10, 20]
       end
+      
+      it "should reject invalid values" do
+        @it = RiCal::RecurrenceRule.new(:freq => "daily", :by_second => [-1, 0, 59, 60])
+        @it.should_not be_valid
+        @it.errors.should == ['-1 is invalid for by_second', '60 is invalid for by_second']
+      end
     end
 
-    
     describe "by_minute parameter" do
 
       it "should accept a single integer" do
@@ -73,6 +87,176 @@ describe RiCal::RecurrenceRule do
       it "should accept an array of integers" do
         @it = RiCal::RecurrenceRule.new(:freq => "daily", :by_minute => [10, 20])
         @it.send(:by_list)[:by_minute].should == [10, 20]
+      end
+
+      it "should reject invalid values" do
+        @it = RiCal::RecurrenceRule.new(:freq => "daily", :by_minute => [-1, 0, 59, 60])
+        @it.should_not be_valid
+        @it.errors.should == ['-1 is invalid for by_minute', '60 is invalid for by_minute']
+      end
+    end
+
+    describe "by_hour parameter" do
+
+      it "should accept a single integer" do
+        @it = RiCal::RecurrenceRule.new(:freq => "daily", :by_hour => 10)
+        @it.send(:by_list)[:by_hour].should == [10]
+      end
+
+      it "should accept an array of integers" do
+        @it = RiCal::RecurrenceRule.new(:freq => "daily", :by_hour => [10, 12])
+        @it.send(:by_list)[:by_hour].should == [10, 12]
+      end
+
+      it "should reject invalid values" do
+        @it = RiCal::RecurrenceRule.new(:freq => "daily", :by_hour => [-1, 0, 23, 24])
+        @it.should_not be_valid 
+        @it.errors.should == ['-1 is invalid for by_hour', '24 is invalid for by_hour']
+      end
+    end
+
+    describe "by_day parameter" do
+      
+      it "should accept a single value" do
+        @it = RiCal::RecurrenceRule.new(:freq => "daily", :by_day => 'MO')
+        @it.send(:by_list)[:by_day].should == [AnyMonday]
+      end
+
+      it "should accept an array of values" do
+        @it = RiCal::RecurrenceRule.new(:freq => "daily", :by_day => ['MO', 'WE'])
+        @it.send(:by_list)[:by_day].should == [AnyMonday, AnyWednesday]
+      end
+
+      it "should reject invalid values" do
+        @it = RiCal::RecurrenceRule.new(:freq => "daily", :by_day => ['VE'])
+        @it.should_not be_valid 
+        @it.errors.should == ['"VE" is not a valid day']
+      end
+    end
+
+    describe "by_month_day parameter" do
+      
+      it "should accept a single value" do
+        @it = RiCal::RecurrenceRule.new(:freq => "daily", :by_month_day => 1)
+        @it.send(:by_list)[:by_month_day].should == [FirstOfMonth]
+      end
+
+      it "should accept an array of values" do
+        @it = RiCal::RecurrenceRule.new(:freq => "daily", :by_month_day => [1, 10])
+        @it.send(:by_list)[:by_month_day].should == [FirstOfMonth, TenthOfMonth]
+      end
+
+      it "should reject invalid values" do
+        @it = RiCal::RecurrenceRule.new(:freq => "daily", :by_month_day => [0, 32, 'VE'])
+        @it.should_not be_valid
+        @it.errors.should == ['0 is not a valid month day','32 is not a valid month day', '"VE" is not a valid month day']
+      end
+    end
+
+    describe "by_year_day parameter" do
+      
+      it "should accept a single value" do
+        @it = RiCal::RecurrenceRule.new(:freq => "daily", :by_year_day => 1)
+        @it.send(:by_list)[:by_year_day].should == [FirstOfYear]
+      end
+
+      it "should accept an array of values" do
+        @it = RiCal::RecurrenceRule.new(:freq => "daily", :by_year_day => [1, 10])
+        @it.send(:by_list)[:by_year_day].should == [FirstOfYear, TenthOfYear]
+      end
+
+      it "should reject invalid values" do
+        @it = RiCal::RecurrenceRule.new(:freq => "daily", :by_year_day => [0, 370, 'VE'])
+        @it.should_not be_valid
+        @it.errors.should == ['0 is not a valid year day', '370 is not a valid year day', '"VE" is not a valid year day']
+      end
+    end
+
+    describe "by_week_no parameter" do
+      
+      it "should accept a single value" do
+        @it = RiCal::RecurrenceRule.new(:freq => "daily", :by_week_no => 2)
+        @it.send(:by_list)[:by_week_no].should == [SecondWeekOfYear]
+      end
+
+      it "should accept an array of values" do
+        @it = RiCal::RecurrenceRule.new(:freq => "daily", :by_week_no => [2, -1])
+        @it.send(:by_list)[:by_week_no].should == [SecondWeekOfYear, LastWeekOfYear]
+      end
+
+      it "should reject invalid values" do
+        @it = RiCal::RecurrenceRule.new(:freq => "daily", :by_week_no => [0, 54, 'VE'])
+        @it.should_not be_valid
+        @it.errors.should == ['0 is not a valid week number', '54 is not a valid week number', '"VE" is not a valid week number']
+      end
+    end 
+    
+    describe "by_month parameter" do
+
+      it "should accept a single integer" do
+        @it = RiCal::RecurrenceRule.new(:freq => "daily", :by_month => 10)
+        @it.send(:by_list)[:by_month].should == [10]
+      end
+
+      it "should accept an array of integers" do
+        @it = RiCal::RecurrenceRule.new(:freq => "daily", :by_month => [10, 12])
+        @it.send(:by_list)[:by_month].should == [10, 12]
+      end
+
+      it "should reject invalid values" do
+        @it = RiCal::RecurrenceRule.new(:freq => "daily", :by_month => [-1, 0, 1, 12, 13])
+        @it.should_not be_valid 
+        @it.errors.should == ['-1 is invalid for by_month', '0 is invalid for by_month', '13 is invalid for by_month']
+      end
+    end
+
+    describe "by_setpos parameter" do
+
+      it "should accept a single integer" do
+        @it = RiCal::RecurrenceRule.new(:freq => "daily", :by_month => 10, :by_setpos => 2)
+        @it.send(:by_list)[:by_setpos].should == [2]
+      end
+
+      it "should accept an array of integers" do
+        @it = RiCal::RecurrenceRule.new(:freq => "daily", :by_month => 10, :by_setpos => [2, 3])
+        @it.send(:by_list)[:by_setpos].should == [2, 3]
+      end
+
+      it "should reject invalid values" do
+        @it = RiCal::RecurrenceRule.new(:freq => "daily", :by_month => 10, :by_setpos => [-367, -366, -1, 0, 1, 366, 367])
+        @it.should_not be_valid 
+        @it.errors.should == ['-367 is invalid for by_setpos', '0 is invalid for by_setpos', '367 is invalid for by_setpos']
+      end 
+      
+      it "should require another BYxxx rule part" do
+        @it = RiCal::RecurrenceRule.new(:freq => "daily", :by_setpos => 2)
+        @it.should_not be_valid
+        @it.errors.should == ['by_setpos cannot be used without another by_xxx rule part']
+      end
+    end
+
+    describe "wkst parameter" do
+
+      it "should default to MO" do
+        @it = RiCal::RecurrenceRule.new(:freq => "daily")
+        @it.wkst.should == 'MO'
+      end
+
+      it "should accept a single string" do
+        @it = RiCal::RecurrenceRule.new(:freq => "daily", :wkst => 'SU')
+        @it.wkst.should == 'SU'
+      end
+      
+      %w{MO TU WE TH FR SA SU}.each do |valid|
+        it "should accept #{valid} as a valid value" do
+          RiCal::RecurrenceRule.new(:freq => "daily", :wkst => valid).should be_valid
+        end
+      end
+      
+      it "should reject invalid values" do
+        @it = RiCal::RecurrenceRule.new(:freq => "daily", :wkst => "bogus")
+        @it.should_not be_valid
+        @it.errors.should == ['"bogus" is invalid for wkst']
       end
     end
 
@@ -99,4 +283,8 @@ describe RiCal::RecurrenceRule do
 
   describe "initialized from string" do
   end
+end
+
+describe RiCal::RecurrenceRule::RecurringDay do
+  it "should have its computation behavior specified"
 end
