adca6e47d20b3b690036384313cbc7a63cd2bae7
Rick DeNatale <rick.denatale@gmail.com>
2009-02-04 13:49:04 -0500

    added initial vtdodo_spec

---
+++ spec/ri_cal/vtodo_spec.rb
@@ -0,0 +1,58 @@
+require File.join(File.dirname(__FILE__), %w[.. spec_helper])
+
+describe RiCal::Vtodo do
+
+  describe "with both due and duration specified" do
+    before(:each) do
+      @it = RiCal::Vtodo.parse_string("BEGIN:VTODO\nDUE:19970903T190000Z\nDURATION:H1\nEND:VTODO").first
+    end
+    
+    it "should be invalid" do
+      @it.should_not be_valid
+    end
+  end
+
+  describe "with a duration property" do
+    before(:each) do
+      @it = RiCal::Vtodo.parse_string("BEGIN:VTODO\nDURATION:H1\nEND:VTODO").first
+    end
+
+    it "should have a duration property" do
+      @it.duration_property.should be
+    end
+    
+    it "should have a duration of 1 Hour" do
+      @it.duration_property.value.should == "H1"
+    end
+    
+    it "should reset the duration property if the due property is set" do
+      @it.due_property = "19970101".to_ri_cal_date_time_value
+      @it.duration_property.should be_nil
+    end
+    
+    it "should reset the duration property if the dtend ruby value is set" do
+      @it.due = "19970101"
+      @it.duration_property.should == nil
+    end
+  end
+
+  describe "with a due property" do
+    before(:each) do
+      @it = RiCal::Vtodo.parse_string("BEGIN:VTODO\nDUE:19970903T190000Z\nEND:VTODO").first
+    end
+
+    it "should have a due property" do
+      @it.due_property.should be
+    end
+    
+    it "should reset the due property if the duration property is set" do
+      @it.duration_property = "H1".to_ri_cal_duration_value
+      @it.due_property.should be_nil
+    end
+    
+    it "should reset the duration property if the dtend ruby value is set" do
+      @it.duration = "19970101"
+      @it.due_property.should == nil
+    end
+  end
+end
