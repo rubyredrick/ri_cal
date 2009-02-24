046e17936678e58bf5b988e85c9a03025489f697
Rick DeNatale <rick.denatale@gmail.com>
2009-02-23 20:13:07 -0500

    Moved inner classes inside RecurrenceRule to separate files

---
+++ lib/ri_cal/property_value/recurrence_rule/enumerator.rb
@@ -0,0 +1,63 @@
+module RiCal
+  class PropertyValue
+    class RecurrenceRule < PropertyValue
+      class Enumerator
+        attr_accessor :start_time, :duration, :next_time, :recurrence_rule
+        attr_reader :reset_second, :reset_minute, :reset_hour, :reset_day, :reset_month
+        def initialize(recurrence_rule, component, setpos_list)
+          self.recurrence_rule = recurrence_rule
+          self.start_time = component.default_start_time
+          self.duration = component.default_duration
+          self.next_time = recurrence_rule.adjust_start(self.start_time)
+          @count = 0
+          @setpos_list = setpos_list
+          @setpos = 1
+          @reset_second = recurrence_rule.reset_second || start_time.sec
+          @reset_minute = recurrence_rule.reset_minute || start_time.min
+          @reset_hour = recurrence_rule.reset_hour || start_time.hour
+          @reset_day = recurrence_rule.reset_day || start_time.day
+          @reset_month = recurrence_rule.reset_month || start_time.month
+          @next_occurrence_count = 0
+        end
+
+        def result_hash(date_time_value)
+          {:start => date_time_value, :end => nil}
+        end
+
+        def result_passes_setpos_filter?(result)
+          result_setpos = @setpos
+          if recurrence_rule.in_same_set?(result, next_time)
+            @setpos += 1
+          else
+            @setpos = 1
+          end
+          if (result == start_time) || (result > start_time && @setpos_list.include?(result_setpos))
+            return true
+          else
+            return false
+          end
+        end
+
+        def result_passes_filters?(result)
+          if @setpos_list
+            result_passes_setpos_filter?(result)
+          else 
+            result >= start_time
+          end
+        end
+
+        def next_occurrence
+          while true
+            @next_occurrence_count += 1
+            result = next_time
+            self.next_time = recurrence_rule.advance(result, self)
+            if result_passes_filters?(result)
+              @count += 1              
+              return recurrence_rule.exhausted?(@count, result) ? nil : result_hash(result)
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ lib/ri_cal/property_value/recurrence_rule/recurring_year_day.rb
@@ -0,0 +1,30 @@
+module RiCal
+  class PropertyValue
+    class RecurrenceRule < PropertyValue
+      class RecurringYearDay < NumberedSpan
+
+        def last
+          366
+        end
+
+        def leap_year?(year)
+          year % 4 == 0 && (year % 400 == 0 || year % 100 != 0)
+        end 
+
+
+        def length_of_year(year)
+          leap_year?(year) ? 366 : 365
+        end 
+
+        def include?(date_or_time)
+          if @source > 0
+            target = @source
+          else
+            target = length_of_year(date_or_time.year) + @source + 1
+          end
+          date_or_time.yday == target
+        end
+      end
+    end
+  end
+end
