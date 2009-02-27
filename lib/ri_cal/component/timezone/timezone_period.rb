7c5178a6ebf65b38ece9d8f23659d3985f2fa13b
Rick DeNatale <rick.denatale@gmail.com>
2009-02-27 17:57:02 -0500

    initial timezone enumeration working

--- lib/ri_cal/component/timezone/timezone_period.rb
+++ lib/ri_cal/component/timezone/timezone_period.rb
@@ -7,6 +7,33 @@ module RiCal
       # in effect.
       class TimezonePeriod < Component
         include Properties::TimezonePeriod
+        
+        include OccurrenceEnumerator
+        
+        def dtend
+          nil
+        end
+        
+        def exdate_property
+          nil
+        end
+        
+        def exrule_property
+          nil
+        end
+        
+        def last_before_utc(utc_time)
+          last_before_local(utc_time + tzoffsetfrom_property)
+        end
+        
+        def last_before_local(local_time)
+          result = nil
+          each do |occurrence|
+            return result if occurrence.dtstart_property >= local_time
+            result = occurrence
+          end
+          return result
+        end
       end
     end
   end
--- spec/ri_cal/component/timezone_spec.rb
+++ spec/ri_cal/component/timezone_spec.rb
@@ -7,4 +7,48 @@ describe RiCal::Component::Timezone do
       RiCal::Component::Timezone.entity_name.should == "VTIMEZONE"
     end
   end
+  
+  describe "from an iCal.app calendar for America/New_York starting March 11 2007" do
+    before(:each) do
+      @it = RiCal.parse_string <<TZEND
+BEGIN:VTIMEZONE
+TZID:US/Eastern
+BEGIN:DAYLIGHT
+TZOFFSETFROM:-0500
+TZOFFSETTO:-0400
+DTSTART:20070311T020000
+RRULE:FREQ=YEARLY;BYMONTH=3;BYDAY=2SU
+TZNAME:EDT
+END:DAYLIGHT
+BEGIN:STANDARD
+TZOFFSETFROM:-0400
+TZOFFSETTO:-0500
+DTSTART:20071104T020000
+RRULE:FREQ=YEARLY;BYMONTH=11;BYDAY=1SU
+TZNAME:EST
+END:STANDARD
+END:VTIMEZONE
+TZEND
+      @it = @it.first
+    end
+    
+    it "should be a Timezone" do
+      @it.should be_kind_of(RiCal::Component::Timezone)
+    end
+    
+    describe ".local_to_utc" do
+      it "should produce 2/27/2009 18:00 UTC for 2/27/2009 13:00" do
+        expected = RiCal::PropertyValue::DateTime.new(:value => "20090227T1800Z" )
+        @it.local_to_utc(DateTime.parse("2/27/2009 13:00")).should == expected
+      end
+    end
+    
+    describe ".utc_to_local" do
+      it "should produce 2/27/2009 13:00 EST for 2/27/2009 18:00 UTC" do
+        expected = RiCal::PropertyValue::DateTime.new(:value => "20090227T1300Z" )
+        @it.utc_to_local(DateTime.parse("2/27/2009 18:00")).should == expected
+      end
+    end
+    
+  end
 end
