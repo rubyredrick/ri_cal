bf4e9f7111923098199adfd799e7b82563bf7607
Rick DeNatale <rick.denatale@gmail.com>
2009-02-28 15:29:11 -0500

    tabled experiment

--- lib/ri_cal/property_value/date.rb
+++ lib/ri_cal/property_value/date.rb
@@ -24,6 +24,30 @@ module RiCal
           @date_time_value = ::DateTime.parse(val.strftime("%Y%m%d"))
         end
       end
+      
+      def year
+        @date_time_value.year
+      end
+      
+      def month
+        @date_time_value.month
+      end
+      
+      def day
+        @date_time_value.day
+      end
+      
+      def year
+        @date_time_value.year
+      end
+      
+      def month
+        @date_time_value.month
+      end
+      
+      def day
+        @date_time_value.day
+      end
 
       # Used by RiCal specs - returns a Ruby Date
       def to_ri_cal_ruby_value
@@ -39,7 +63,33 @@ module RiCal
       def to_ri_cal_date_value
         self
       end
+      
+      def compute_change(d, options) # :nodoc:
+        ::Date.civil((options[:year] || d.year), (options[:month] || d.month), (options[:day] || d.day))
+      end
 
+      def compute_advance(d, options) # :nodoc:
+        d = d >> options[:years] * 12 if options[:years]
+        d = d >> options[:months]     if options[:months]
+        d = d +  options[:weeks] * 7  if options[:weeks]
+        d = d +  options[:days]       if options[:days]
+        compute_change(@date_time_value, :year => d.year, :month => d.month, :day => d.day)
+      end
+
+      def advance(options) # :nodoc:
+        PropertyValue::Date.new(:value => compute_advance(@date_time_value, options), :params =>(params ? params.dup : nil) )
+      end
+
+      def change(options) # :nodoc:
+        PropertyValue::Date.new(:value => compute_change(@date_time_value, options), :params => (params ? params.dup : nil) )
+      end
+
+      # Delegate unknown messages to the wrappered Date instance.
+      # TODO: Is this really necessary?
+      def method_missing(selector, *args)
+        @date_value.send(selector, *args)
+      end
+      
       # TODO: consider if this should be a period rather than a hash
       def occurrence_hash(default_duration) # :nodoc:
         date_time = self.to_ri_cal_date_time_value
