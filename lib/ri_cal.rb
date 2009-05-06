e1f5ecab4104ec4fd09c48025bd497d7070c488e
Rick DeNatale <rick.denatale@gmail.com>
2009-05-06 13:00:40 -0400

    fixed manifest removed uncommitted occurrence_list_spec

--- lib/ri_cal.rb
+++ lib/ri_cal.rb
@@ -12,7 +12,7 @@ module RiCal
   autoload :OccurrenceEnumerator, "#{my_dir}/ri_cal/occurrence_enumerator.rb"
   
   # :stopdoc:
-  VERSION = '0.0.2'
+  VERSION = '0.0.3'
   LIBPATH = ::File.expand_path(::File.dirname(__FILE__)) + ::File::SEPARATOR
   PATH = ::File.dirname(LIBPATH) + ::File::SEPARATOR
 
