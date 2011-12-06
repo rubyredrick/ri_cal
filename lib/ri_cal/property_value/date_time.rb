cb5606711cde9fc242c585042281693dd4c4c589
Benoist Claassen <bclaassen@digidentity.eu>
2011-12-06 15:06:54 +0100

    changed get_zone to find_zone! if rails is v3.1.x

--- lib/ri_cal/property_value/date_time.rb
+++ lib/ri_cal/property_value/date_time.rb
@@ -307,7 +307,8 @@ module RiCal
       # Returns a ruby DateTime object representing the receiver.
       def ruby_value
         if has_valid_tzinfo_tzid? && RiCal::TimeWithZone && tz_info_source?
-          RiCal::TimeWithZone.new(utc.to_datetime, ::Time.__send__(:get_zone, @tzid))
+          time_zone_meth = ::Time.respond_to?(:find_zone!) ? :find_zone! : :get_zone
+          RiCal::TimeWithZone.new(utc.to_datetime, ::Time.__send__(time_zone_meth, @tzid))
         else
           ::DateTime.civil(year, month, day, hour, min, sec, rational_tz_offset).set_tzid(@tzid)
         end
