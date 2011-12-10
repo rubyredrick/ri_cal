9161721708297cc0017bd9ad2d490867e9bf9f29
Brent Miller <foliosus@foliosus.com>
2011-12-10 12:35:56 -0800

    Fix for extra colon at beginning of x-properties when written to string

--- lib/ri_cal/component.rb
+++ lib/ri_cal/component.rb
@@ -214,7 +214,7 @@ module RiCal
     def export_x_properties_to(export_stream) #:nodoc:
       x_properties.each do |name, props|
         props.each do | prop |
-          export_stream.puts("#{name}:#{prop}")
+          export_stream.puts("#{name}#{prop}")
         end
       end
     end
